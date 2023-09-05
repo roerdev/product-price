ae2667ef2f7b4e3ea9dff57e72c09391af1e95b5
Erick Rodriguez <erodriguezven@gmail.com>
2023-09-05 22:27:36 +0200

    Controller Handler

---
+++ src/main/java/com/productprice/exceptions/handler/RestApiResponseEntityExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.productprice.exceptions.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class RestApiResponseEntityExceptionHandler {
+
+    //TODO: Implementar metodos del handler para manejar respuestas de error ante una Excepción
+}
